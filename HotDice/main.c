d0d4e259b5f82f470e34d085ff7b220d11566cf1
Matthew Rand <mrand@moleman.local>
2019-07-17 07:59:15 -0500

    Initial Commit

---
+++ HotDice/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ * HotDice
+ *
+ * Created by Matthew Rand on 2019-07-17.
+ * Copyright (c) 2019 Matthew Rand. All rights reserved.
+ *
+ */
+
+
+#include <stdio.h>
+#include <conio.h>
+
+
+int main(void)
+{
+    printf("HELLO, WORLD!\n");
+    cgetc();
+    return 0;
+}
